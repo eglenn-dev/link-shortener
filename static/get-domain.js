70b8f0cd6ed499291d6ef14bb929ee134cce35e5
Ethan Glenn <ethan+git@eglenn.dev>
2023-12-29 19:04:50 -0700

    Added line comments

--- static/get-domain.js
+++ static/get-domain.js
@@ -1,28 +1,30 @@
 function getDomain() {
-    var url = window.location.href;
-    var domain = new URL(url).hostname;
-    return domain;
+    // Return the domain that the project is running on. 
+    return window.location.hostname;
 }
 
 function updatePageContent() {
+    // Gets the page domain.
     var domain = getDomain();
+    // Getting the element to host the domain URL.
     var linkElement = document.getElementById("entire-url");
     if (domain && linkElement) {
+        // Setting the text content of the element to the domain. 
         linkElement.textContent = domain;
     }
 }
 
 function copyToClipboard() {
     // Get the text field
-  var copyText = document.getElementById("whole-url");
+    var copyText = document.getElementById("whole-url");
 
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
 
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
 
-  // Alert the copied text
-  alert("Copied the text: " + copyText.value);
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
 }
